f6b9ecb03fdb3fdb1e06330a3249367a4ca20237
jhochmuth <jhochmuth@comcast.net>
2019-05-30 14:07:47 -0600

    Data preparation.

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,5 @@
+import os
+
 import py_midicsv as midi
 
 
@@ -7,17 +9,27 @@ def midi_command_formatting(command):
     return command
 
 
-def midi_to_csv(file):
-    csv = midi.midi_to_csv(file)
+def midi_to_csv(filename):
+    csv = midi.midi_to_csv(filename)
     return [midi_command_formatting(command) for command in csv]
 
 
-# TODO: Replace list comprehension with normal loop so that you can break after finishing with notes.
 def extract_note_values(midi_command_list):
-    return [command[4] for command in midi_command_list if command[2] == "Note_on_c"]
+    return [command[4] for command in midi_command_list
+            if command[2] == "Note_on_c" and int(command[5]) > 0 and int(command[3]) == 0]
 
 
-midi_command_list = midi_to_csv("data/midi_files/bach_suite1_i.mid")
-print(midi_command_list)
-note_list = extract_note_values(midi_command_list)
-print(note_list)
+def extract_notes_from_file(filename):
+    midi_command_list = midi_to_csv(filename)
+    return extract_note_values(midi_command_list)
+
+
+def extract_notes_from_all_files(directory):
+    pieces = list()
+    for filename in os.listdir(directory):
+        if filename.endswith(".mid"):
+            pieces.append(extract_notes_from_file(os.path.join(directory, filename)))
+    return pieces
+
+
+pieces = extract_notes_from_all_files("data/midi_files")
